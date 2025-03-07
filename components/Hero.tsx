f843a9efd545b49b43e17d9239058bbac2494112
Emmanuel oshakpemeh <149076550+imacul@users.noreply.github.com>
2025-03-07 07:28:13 +0000

    security

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
           Streamline your car rental experience with our effortless booking process.
         </p>
         <CustomButton
-        title="Explore Cars"
+        title="Explore Cars!"
         containerStyles="mb-10 bg-primary-blue text-white rounded-full mt-10 transition hover:scale-125"
         handleClick={handleScroll}
          />
